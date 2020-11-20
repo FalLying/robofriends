04f7156d05e74c341bb99e1ba5276d6306b2e598
Jonas Brito <jonasexplore@gmail.com>
2020-11-20 09:44:18 -0300

    react hooks.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,45 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../componets/CardList';
 import SearchBox from '../componets/SearchBox';
 import './App.css';
 import Scroll from '../componets/Scroll';
 
-class App extends Component {
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchfileld: ''
-        }
-    }
+function App() {
+    const [robots, setRobot] = useState([])
+    const [searchField, setSearchField] = useState('')
 
-    componentDidMount(){
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({robots: users}));
-    }
+            .then(users => setRobot(users));
+    }, [])
     
-    onSearchChange = (e) => {
-        this.setState({searchfileld: e.target.value});
+    const onSearchChange = (e) => {
+        setSearchField(e.target.value);
     }
 
-    render(){
-        const {robots, searchfileld} = this.state;
-        const filteredRobots = robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(searchfileld.toLowerCase());
-        })
-        return !robots.length ?
-            <p className='tc'>Ooooh! Looks that you don't have any robot!</p>:
-            (
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <CardList robots={filteredRobots} />
-                    </Scroll>
-                </div>
-            );
-    }
+    const filteredRobots = robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+    return !robots.length ?
+        <p className='tc'>Ooooh! Looks that you don't have any robot!</p>:
+        (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
+            </div>
+        );
+
 }
 
 export default App;
